2e017925b89f76e8a04ac196f5fb0916e80db6e6
jayanth-caw <jayanth.reddy@caw.tech>
2025-07-08 16:31:56 +0530

    added

--- class1.js
+++ class1.js
@@ -44,7 +44,7 @@ export class ShoppingCart {
     updateQuantity(itemId, quantity) {
         const item = this.items.find(item => item.id === itemId);
         if (item) {
-            item.quantity = Math.max(1, newQuantity);
+            item.quantity = Math.max(1, quantity);
             this.updateTotal();
         }
 
